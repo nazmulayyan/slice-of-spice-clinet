66e938224b97b409f8cd93b6652269df89a9ee42
NazmulAyyan <rnazmul2424@gmail.com>
2023-05-04 14:10:41 +0600

    login page added

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -3,7 +3,36 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h3>log in</h3>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col ">
+                    <div className="text-center lg:text-left">
+                        <h3 className="text-3xl font-bold uppercase">If you have an account </h3>
+                        <h3 className="text-3xl font-bold uppercase text-center">please login</h3>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn bg-rose-500">Login</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
